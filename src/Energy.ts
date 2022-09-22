c0b202d82983bc446e0f04c7e778cbcc7a7fdfbf
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-09-22 18:59:24 -0300

    criação da interface Fighter

--- src/Energy.ts
+++ src/Energy.ts
@@ -1,11 +1,6 @@
-// export enum EnergyType { mana, stamina}
-
 export type EnergyType = 'mana' | 'stamina';
 
 export default interface Energy {
   type_: EnergyType;
   amount: number;
 }
-
-// const testEnergyType1: EnergyType = 'mana'; // OK
-// const testEnergyType2: EnergyType = 'qualquer coisa'; // erro
